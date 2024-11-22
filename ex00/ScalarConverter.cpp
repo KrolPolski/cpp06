1e0482afeda83030a7bb30d159378d3bba31224a
Ryan Boudwin <rboudwin@student.hive.fi>
2024-11-22 10:51:52 +0200

    ex00 complete

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:39:48 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/11/22 10:26:22 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/11/22 10:50:54 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-static bool isChar(std::string str)
+bool ScalarConverter::isChar(std::string str)
 {
 	if ((isalpha(str[0]) && str.length() == 1)
 		|| (isalpha(str[1]) && str.length() == 3 && str[0] == '\'' && str[2] == '\''))
@@ -29,7 +29,7 @@ static bool isChar(std::string str)
 		return false;
 }
 
-static bool isInt(std::string str)
+bool ScalarConverter::isInt(std::string str)
 {
 	for (size_t i = 0; i < str.length(); i++)
 	{
@@ -38,7 +38,7 @@ static bool isInt(std::string str)
 	}
 	return true;
 }
-static bool isFloat(std::string str)
+bool ScalarConverter::isFloat(std::string str)
 {
 	bool decimalFound {false};
 	for (size_t i = 0; i < str.length() - 1; i++)
@@ -59,7 +59,7 @@ static bool isFloat(std::string str)
 		return false;
 }
 
-static bool isDouble(std::string str)
+bool ScalarConverter::isDouble(std::string str)
 {
 	bool decimalFound {false};
 	for (size_t i = 0; i < str.length(); i++)
@@ -77,7 +77,7 @@ static bool isDouble(std::string str)
 	return true;
 }
 
-static void printFloat(float numf, std::string str)
+void ScalarConverter::printFloat(float numf, std::string str)
 {
 	unsigned int precision {1};
 	unsigned int len;
@@ -96,7 +96,7 @@ static void printFloat(float numf, std::string str)
 	std::cout << "float: " << std::fixed << std::setprecision(precision) << numf << "f" << std::endl;
 }
 
-static void printDouble(double numd, std::string str)
+void ScalarConverter::printDouble(double numd, std::string str)
 {
 	unsigned int precision {1};
 	unsigned int len;
@@ -114,7 +114,7 @@ static void printDouble(double numd, std::string str)
 	std::cout << "double: " << std::fixed << numd << std::endl;
 }
 
-static void printChar(int num)
+void ScalarConverter::printChar(int num)
 {
 	if (num < -128 || num > 127)
 		std::cout << "char: impossible" << std::endl;
@@ -133,58 +133,62 @@ void ScalarConverter::convert(std::string str)
 	float		numf;
 	double		numd;
 	
-	//special cases of -inff and +inff and nanf
 	if (str == "-inff")
 	{
 		type = FLOAT;
 		numf = -std::numeric_limits<float>::infinity();
-		std::cout << "Char: Impossible" << std::endl;
-		std::cout << "Int: Impossible" << std::endl;
-		std::cout << "Float: " << numf << std::endl;
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: " << numf << std::endl;
 		numd = -std::numeric_limits<double>::infinity();
-		std::cout << "Double: " << numd << std::endl;
+		std::cout << "double: " << numd << std::endl;
 		return ;
 	}
 	else if (str == "+inff")
 	{
 		type = FLOAT;
 		numf = std::numeric_limits<float>::infinity();
-		std::cout << "Char: Impossible" << std::endl;
-		std::cout << "Int: Impossible" << std::endl;
-		std::cout << "Float: " << numf << std::endl;
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: " << numf << std::endl;
 		numd = std::numeric_limits<double>::infinity();
-		std::cout << "Double: " << numd << std::endl;
+		std::cout << "double: " << numd << std::endl;
 		return ;
 	}
-	// else if (str == "nanf")
-	
-	//special cases of -inf, +inf and nan
+	else if (str == "nanf" || str == "nan")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		numf = std::numeric_limits<float>::has_quiet_NaN;
+		numd = std::numeric_limits<double>::has_quiet_NaN;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+		return ;
+	}
 
 	else if (str == "-inf")
 	{
 		type = DOUBLE;
 		numd = -std::numeric_limits<double>::infinity();
-		std::cout << "Char: Impossible" << std::endl;
-		std::cout << "Int: Impossible" << std::endl;
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
 		numf = -std::numeric_limits<float>::infinity();
-		std::cout << "Float: " << numf << std::endl;
-		std::cout << "Double: " << numd << std::endl;
+		std::cout << "float: " << numf << std::endl;
+		std::cout << "double: " << numd << std::endl;
 		return ;
 	}
 	else if (str == "+inf")
 	{
 		type = DOUBLE;
 		numd = std::numeric_limits<double>::infinity();
-		std::cout << "Char: Impossible" << std::endl;
-		std::cout << "Int: Impossible" << std::endl;
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
 		numf = std::numeric_limits<float>::infinity();
-		std::cout << "Float: " << numf << std::endl;
-		std::cout << "Double: " << numd << std::endl;
+		std::cout << "float: " << numf << std::endl;
+		std::cout << "double: " << numd << std::endl;
 		return ;
 	}
-	// else if (str == "nan")
 	
-	//detect if str is a char
 	else if (isChar(str))
 	{
 		type = CHAR;
@@ -192,23 +196,18 @@ void ScalarConverter::convert(std::string str)
 			c = str[1];
 		else
 			c = str[0];
-		std::cout << "this is a char" << std::endl;
 	}
-	//detect if str is an int
 	else if (isInt(str))
 	{
 		type = INT;
-		std::cout << "This is an int" << std::endl;
 	}
 	else if (isFloat(str))
 	{
 		type = FLOAT;
-		std::cout << "This is a float" << std::endl;
 	}
 	else if (isDouble(str))
 	{
 		type = DOUBLE;
-		std::cout << "This is a double" << std::endl;
 	}
 	else
 	{
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:39:40 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/11/22 10:21:50 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/11/22 10:49:32 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,21 @@
 
 class ScalarConverter
 {
-private:
-	ScalarConverter();
-	enum e_type {CHAR, INT, FLOAT, DOUBLE};
-	
-public:
-	ScalarConverter(ScalarConverter& other) = delete;
-	ScalarConverter& operator=(ScalarConverter& other) = delete;
-	~ScalarConverter();
-	static void convert(std::string str);
+	private:
+		ScalarConverter();
+		ScalarConverter(ScalarConverter& other) = delete;
+		ScalarConverter& operator=(ScalarConverter& other) = delete;
+		~ScalarConverter();
+		enum e_type {CHAR, INT, FLOAT, DOUBLE};
+		static bool isChar(std::string str);
+		static bool isDouble(std::string str);
+		static bool isFloat(std::string str);
+		static bool isInt(std::string str);
+		static void printChar(int num);
+		static void printDouble(double numd, std::string str);
+		static void printFloat(float numf, std::string str);
+		
+	public:
+		static void convert(std::string str);
 };
 
